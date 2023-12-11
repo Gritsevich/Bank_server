97667c51e86a9b2c1184600909efbe0a2a2da213
Gritsevich <gritsevich.rina@gmail.com>
2023-12-11 06:53:05 +0300

    revert

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,18 +11,17 @@ module.exports = async function(req, res, next)
   try
   {
     const token = req.headers.authorization.split(' ')[1] // Bearer hbrjdbdnbjn
-    // if(!token)
-    // {
-      // return res.status(401).json({message: "Не авторизован"})
-    // }
+    if(!token)
+    {
+      return res.status(401).json({message: "Не авторизован"})
+    }
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
     req.user = await User.findOne({where:{ id: decoded.id }});
-    // if(!req.user)
-      // return res.status(401).json({message: "Не авторизован"}) 
+    if(!req.user)
+      return res.status(401).json({message: "Не авторизован"}) 
     next()
   } catch(e)
   {
-    next()
-    // res.status(401).json({message: "Не авторизован"}) 
+    res.status(401).json({message: "Не авторизован"}) 
   }
 };
