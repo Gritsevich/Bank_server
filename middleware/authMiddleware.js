6e547ef4bb995c125ad9ab1130243de2c8513cd2
Gritsevich <gritsevich.rina@gmail.com>
2023-12-11 06:54:24 +0300

    check error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,17 +11,20 @@ module.exports = async function(req, res, next)
   try
   {
     const token = req.headers.authorization.split(' ')[1] // Bearer hbrjdbdnbjn
+    console.log(token)
     if(!token)
     {
       return res.status(401).json({message: "Не авторизован"})
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
     req.user = await User.findOne({where:{ id: decoded.id }});
+    console.log(req.user)
     if(!req.user)
       return res.status(401).json({message: "Не авторизован"}) 
     next()
   } catch(e)
   {
+    console.log("CATCH!!!");
     res.status(401).json({message: "Не авторизован"}) 
   }
 };
